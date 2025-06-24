3680550a9b3c7730fab180e7035b09ab33fec81d
HorizonUltima <hug.entertainee@gmail.com>
2025-06-24 20:25:33 +0300

    Fixes continue.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 20:31:10 by hgergink          #+#    #+#             */
-/*   Updated: 2025/05/30 12:04:19 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/24 20:25:12 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	while ((i < size - 1) && (src[i]))
 		dst[i] = src[i++];
 	dst[i] = 0;
-	return (hstrlen(src));
+	return (ft_strlen(src));
 }
