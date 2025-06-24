8a23af3c4f5bd0e96d6a5d9ca487a610b79a8f8d
HorizonUltima <hug.entertainee@gmail.com>
2025-06-24 20:46:11 +0300

    Fixes continue.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 12:28:31 by hgergink          #+#    #+#             */
-/*   Updated: 2025/06/03 13:08:48 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/24 20:46:04 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	if (nmemb == 0 || size == 0)
-		return (malloc(0));
-	if (nmemb > SIZE_MAX / size)
-		return ((void *)0);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return ((void *)0);
