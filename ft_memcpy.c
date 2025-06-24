be5864577a7b221ab8f879db17642ba387b46285
HorizonUltima <hug.entertainee@gmail.com>
2025-06-24 18:28:06 +0300

    General Makefile fixes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 18:58:29 by hgergink          #+#    #+#             */
-/*   Updated: 2025/05/28 19:00:11 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/24 18:27:13 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t			i;
 
 	d = dest;
-	source = src;
+	source = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 		d[i] = source[i++];
