ddded822ccb02770484fc4328156bceef812f686
HorizonUltima <hug.entertainee@gmail.com>
2025-06-25 12:32:57 +0300

    Fixes continue.

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 19:51:42 by hgergink          #+#    #+#             */
-/*   Updated: 2025/06/25 12:30:14 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/25 12:32:44 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ static char	*ft_convey(char const *s, char *str, unsigned int start, size_t len)
 	i = 0;
 	while (i < len)
 	{
-		subs[i] = s[start + i];
+		str[i] = s[start + i];
 		i++;
 	}
 	subs[i] = 0;
-	return (subs);
+	return (str);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
