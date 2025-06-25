730f7ae8ed0a706568e9615a1c7017797cb6d66a
HorizonUltima <hug.entertainee@gmail.com>
2025-06-25 15:07:17 +0300

    strlcat fixed?

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 16:38:44 by hgergink          #+#    #+#             */
-/*   Updated: 2025/05/30 12:02:51 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/25 15:01:44 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
+	size_t	dstlen;
+	size_t	srclen;
 
-	j = ft_strlen(dst);
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstlen >= size)
+		return (size + srclen);
 	i = 0;
-	while (i < size - 1)
-		dst[j++] = src[i++];
-	dst[j] = 0;
-	return (j);
+	while (dstlen + i < size - 1 && src[i])
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = 0;
+	return (dstlen + srclen);
 }
