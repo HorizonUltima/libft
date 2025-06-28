365938923d7b3fa6851bc0a5454138fc93a053f2
HorizonUltima <hug.entertainee@gmail.com>
2025-06-28 12:04:00 +0300

    1

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 15:18:08 by hgergink          #+#    #+#             */
-/*   Updated: 2025/06/27 15:18:17 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/28 11:55:37 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return ((void *) 0);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hgergink <hgergink@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 21:13:15 by hgergink          #+#    #+#             */
-/*   Updated: 2025/06/27 21:13:37 by hgergink         ###   ########.fr       */
+/*   Updated: 2025/06/28 11:50:16 by hgergink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlst;
 	t_list	*lastnode;
 
-	if (!lst || !del)
+	if (!lst || !del || !f)
 		return ((void *)0);
 	newlst = ft_lstnew(f(lst->content));
 	lastnode = newlst;
-	if (!newlst || !newlst->content)
+	if (!newlst)
+	{
 		ft_lstdelone(newlst, del);
+		return ((void *)0);
+	}
 	while (lst->next)
 	{
 		lst = lst->next;
 		lastnode->next = ft_lstnew(f(lst->content));
-		if (!lastnode->next || !lastnode->next->content)
+		if (!lastnode->next)
 		{
 			ft_lstclear(&newlst, del);
 			return ((void *)0);
